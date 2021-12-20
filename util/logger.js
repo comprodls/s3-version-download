4bd7cf45314358d13615928bddb0817411cdc492
Manpreet Singh <manpreet.singh@comprotechnologies.com>
2021-12-20 12:03:00 +0530

    winston logger

--- util/logger.js
+++ util/logger.js
@@ -1,12 +1,33 @@
-const bunyan = require('bunyan');
-const log = bunyan.createLogger({name: 's3Download'});
+const {transports, createLogger, format} = require('winston');
+
+const logConfiguration = {
+  format: format.combine(
+    format.timestamp(),
+    format.json()
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({
+        filename: 'logs/version.log',
+        maxsize: 1000
+    })
+  ]
+};
+
+const logger = createLogger(logConfiguration);
 
  function info(msg) {
-    log.info(msg);
+   logger.log({
+        message: msg,
+        level: 'info'
+    });
   }
 
   function error(msg) {
-    log.error(msg);
+    logger.log({
+      message: msg,
+      level: 'error'
+    });
   }
 
   module.exports = {
